f3cdb8c3166df34e82112e3741f949a84ee90ebc
bymse <egorstrelov@gmail.com>
2023-04-15 17:26:30 +0300

    Lab7

---
+++ OpenMP/Lab7/Lab7.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Enter matrix size" << std::endl;
+    int size;
+
+    std::cin >> size;
+	double** A = new double* [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        A[i] = new double [size];
+        for (int j = 0; j < size; j++)
+        {
+            A[i][j] = (double)rand() / RAND_MAX;
+        }
+    }
+
+    double det = 1;
+#pragma omp parallel for shared(A,det)
+    for (int i = 0; i < size - 1; i++)
+    {
+        int max_row = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (fabs(A[j][i]) > fabs(A[max_row][i]))
+            {
+                max_row = j;
+            }
+        }
+        if (max_row != i)
+        {
+            std::swap(A[i], A[max_row]);
+            det *= -1;
+        }
+
+#pragma omp parallel for shared(A,det)
+        for (int j = i + 1; j < size; j++)
+        {
+            double factor = A[j][i] / A[i][i];
+            for (int k = i + 1; k < size; k++)
+            {
+                A[j][k] -= factor * A[i][k];
+            }
+            A[j][i] = 0;
+        }
+    }
+
+    double det_diag = 1.0;
+#pragma omp parallel for shared(A) reduction(*:det_diag)
+    for (int i = 0; i < size; i++)
+    {
+        det_diag *= A[i][i];
+    }
+    det *= det_diag;
+
+    std::cout << "Determinant of the matrix is " << det << std::endl;
+
+    for (int i = 0; i < size; i++) {
+        delete[] A[i];
+    }
+
+    delete[] A;
+}
+
+
